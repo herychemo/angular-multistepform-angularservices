b6c17a89e7c25ab55df4a4a1ab4b327a375efa14
Heriberto Reyes Esparza <hery.chemo@gmail.com>
2020-11-16 13:20:57 -0600

    Adding Form result Component

---
+++ src/app/components/form-result/form-result.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FormResultComponent } from './form-result.component';
+
+describe('FormResultComponent', () => {
+  let component: FormResultComponent;
+  let fixture: ComponentFixture<FormResultComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FormResultComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FormResultComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
