aa727f68606c21abb4081d5916023f5822aac66f
Heriberto Reyes Esparza <hery.chemo@gmail.com>
2020-11-17 16:23:43 -0600

    Address Details Form Step

--- src/app/components/address-form-step/address-form-step.component.ts
+++ src/app/components/address-form-step/address-form-step.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {UserFormDomainService} from '../../services/user-form-domain.service';
+import {ActivatedRoute, Router} from '@angular/router';
+import {UserFormModel} from '../../model/domain/user-form-model';
 
 @Component({
   selector: 'app-address-form-step',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddressFormStepComponent implements OnInit {
 
-  constructor() { }
+  get userFormModel(): UserFormModel {
+    return this.userFormDomainServiceService.model;
+  }
 
-  ngOnInit(): void {
+  constructor(
+    public userFormDomainServiceService: UserFormDomainService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
+  ngOnInit(): void {}
+
+  goNext(): void {
+    this.router.navigate(['..', 'topics'], { relativeTo: this.route});
   }
 
 }
