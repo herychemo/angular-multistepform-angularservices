ab5bf2252a83da658248113d0cc7e69ca66fcdbe
Heriberto Reyes Esparza <hery.chemo@gmail.com>
2020-11-17 14:28:01 -0600

    User Details Form Step

--- src/app/components/user-form-step/user-form-step.component.ts
+++ src/app/components/user-form-step/user-form-step.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {UserFormDomainServiceService} from '../../services/user-form-domain-service.service';
+import {ActivatedRoute, Router} from '@angular/router';
+import {UserFormModel} from '../../model/domain/user-form-model';
 
 @Component({
   selector: 'app-user-form-step',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserFormStepComponent implements OnInit {
 
-  constructor() { }
+  get userFormModel(): UserFormModel {
+    return this.userFormDomainServiceService.model;
+  }
 
-  ngOnInit(): void {
+  constructor(
+    public userFormDomainServiceService: UserFormDomainServiceService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
+  ngOnInit(): void {}
+
+  goNext(): void {
+    this.router.navigate(['..', 'address'], { relativeTo: this.route});
   }
 
 }
